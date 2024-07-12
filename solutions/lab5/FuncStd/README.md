6dad70aa8e2f712e8241a25a908e2b9856b16288
Iheb KHEMISSI <iheb.khemissi+github@gmail.com>
2024-07-12 10:27:33 +0000

    feat: Add solution for lab5
    Provide a utility function to inject unexpected behaviors into the application.
    
    noissue

---
+++ solutions/lab5/FuncStd/README.md
@@ -0,0 +1,10 @@
+## Simulating unexpected behaviors
+
+Use the following environment variables to inject errors and latency:
+
+| App setting        | Default value | Description                                           |
+|--------------------|---------------|-------------------------------------------------------|
+| ERROR_RATE         | 0             | Percentage of errors. Set a number between 0 and 100. |
+| LATENCY_IN_SECONDS | 0             | Extra latency in seconds                              |
+
+Use Application Insights to investigate these issues.
---
+++ solutions/lab5/FuncStd/UnexpectedBehaviors.cs
@@ -0,0 +1,36 @@
+namespace FuncStd
+{
+    public static class UnexpectedBehaviors
+    {
+        private static int _errorRate = 0;
+        private static int _latencyInSeconds = 0;
+
+        static UnexpectedBehaviors()
+        {
+            // Get the error rate from the environment variables
+            if (!Int32.TryParse(Environment.GetEnvironmentVariable("ERROR_RATE"), out _errorRate))
+            {
+                _errorRate = 0;
+            }
+
+            // Get the extra injected latency from the environment variables
+            if (!Int32.TryParse(Environment.GetEnvironmentVariable("LATENCY_IN_SECONDS"), out _latencyInSeconds))
+            {
+                _latencyInSeconds = 0;
+            }
+        }
+
+        public static void Simulate()
+        {
+            // Simulating latency: sleep for _latencyInSeconds seconds
+            if (_latencyInSeconds != 0) {
+                Thread.Sleep(_latencyInSeconds * 1000);
+            }
+
+            // Simulating errors: throw errors with a probability of _errorRate
+            if (_errorRate != 0 && Random.Shared.Next(0, 100) < _errorRate) {
+                throw new Exception("Simulated error!");
+            }
+        }
+    }
+}
